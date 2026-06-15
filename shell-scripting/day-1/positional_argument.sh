71c89d05b25119ec6b617de40f81464b01cc8c64
Mryash7803 <yashsingar5@gmail.com>
2026-06-15 13:47:54 +0530

    Remove shell-scripting folder from K8s project

--- shell-scripting/day-1/positional_argument.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ls $1 | wc -l > file_list.txt
-
-#count=$(ls "$1" | wc -l)
-#echo "Total files: $count"
--- shell-scripting/day-1/while-loop-ip-example.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while read -r ip; do
-    if ping -c 1 "$ip" > /dev/null 2>&1; then
-        echo "$ip is UP"
-    else
-        echo "$ip is down"
-    fi
-done < servers.txt
